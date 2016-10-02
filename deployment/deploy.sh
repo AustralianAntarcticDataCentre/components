2032627b23ee587c243c9d96219efb4c6af6586d
ANTDIV\james_cus <james.cusick@aad.gov.au>
2016-10-03 10:36:44 +1100

    Updated deployment script

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -1,28 +1,20 @@
 #!/bin/sh
 
-source variables.sh
+docker stop aadc-components
+docker rm  -f aadc-components
 
-deploy_components () {
-	docker stop aadc-components
-	docker rm aadc-components
-
-	echo -e '\n----------------------------------------------------------------'
-	echo -e 'Running the aadc-components container with persistent storage on the host'
-	echo -e '----------------------------------------------------------------'
-	docker run \
-		-d \
-		-v $DIR_MOUNT_GIT:/usr/share/nginx/html \
-		-v $DIR_MOUNT_CONF:/etc/nginx/conf.d/default.conf \
-		-p $COMPONENTS_PORT:80 \
-		--name aadc-components \
-		--restart=always \
-		nginx:latest
+echo -e '\n----------------------------------------------------------------'
+echo -e 'Running the aadc-components container with persistent storage on the host'
+echo -e '----------------------------------------------------------------'
+docker run \
+	-d \
+	-v %env.DIR_GIT%:/usr/share/nginx/html \
+	-v %env.DIR_CONF%:/etc/nginx/conf.d/default.conf \
+	-p %env.APP_PORT%:80 \
+	--name aadc-components \
+	--restart=always \
+	nginx:latest
 }
-
-mkdir -p $DIR_MOUNT_GIT
-
-# Call the function to deploy the application
-deploy_components
 
 # List the Docker containers
 docker ps
